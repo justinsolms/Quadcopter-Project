c8197d4c99202b479807c0b0c502c2579f3c0d78
Justin Solms <justinsolms@gmail.com>
2018-04-05 20:56:21 +0200

    kkweon/DDPG.py - https://gist.github.com/kkweon/a82980f3d60ffce1d69ad6da8af0e124

--- agents/ddpg.py
+++ agents/ddpg.py
@@ -124,6 +124,11 @@ class Critic:
                                   )(states)
         net_states = layers.BatchNormalization()(net_states)
         net_states = layers.LeakyReLU(alpha=0.3)(net_states)
+        net_states = layers.Dense(units=400, activation='linear',
+                                  kernel_initializer=init,
+                                  bias_initializer=init,
+                                  kernel_regularizer=regularizers.l2(0.01)
+                                  )(net_states)
 
         # Add hidden layer(s) for action pathway
         net_actions = layers.Dense(units=400, activation='linear',
@@ -131,23 +136,16 @@ class Critic:
                                    bias_initializer=init,
                                    kernel_regularizer=regularizers.l2(0.01)
                                    )(actions)
-        net_actions = layers.BatchNormalization()(net_actions)
-        net_actions = layers.LeakyReLU(alpha=0.3)(net_actions)
 
         # Try different layer sizes, activations, add batch normalization,
         # regularizers, etc.
 
         # Combine state and action pathways
         net = layers.Add()([net_states, net_actions])
+        net = layers.BatchNormalization()(net)
         net = layers.LeakyReLU(alpha=0.3)(net)
 
         # Add more layers to the combined network if needed
-        net = layers.Dense(units=300, activation='linear',
-                           kernel_initializer=init,
-                           bias_initializer=init,
-                           kernel_regularizer=regularizers.l2(0.01)
-                           )(net)
-        net = layers.LeakyReLU(alpha=0.3)(net)
 
         # Add final linear output layer to prduce action values (Q values)
         init = initializers.RandomUniform(minval=-0.003, maxval=0.003)
