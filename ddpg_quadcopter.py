82e41006b36c92eecdd59b87b39d007415b33b7f
Justin Solms <justinsolms@gmail.com>
2018-04-07 16:13:13 +0200

    Tested QuadCopter-v0 - bad model

--- ddpg_quadcopter.py
+++ ddpg_quadcopter.py
@@ -27,7 +27,7 @@ from rl.memory import SequentialMemory
 from rl.random import OrnsteinUhlenbeckProcess
 
 
-ENV_NAME = 'Pendulum-v0'
+ENV_NAME = 'QuadCopter-v0'
 gym.undo_logger_setup()
 
 
@@ -78,10 +78,10 @@ agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-agent.fit(env, nb_steps=50000, visualize=True, verbose=1, nb_max_episode_steps=200)
+agent.fit(env, nb_steps=50000, visualize=False, verbose=1, nb_max_episode_steps=200)
 
 # After training is done, we save the final weights.
 agent.save_weights('ddpg_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
-agent.test(env, nb_episodes=5, visualize=True, nb_max_episode_steps=200)
+agent.test(env, nb_episodes=5, visualize=False, nb_max_episode_steps=200)
